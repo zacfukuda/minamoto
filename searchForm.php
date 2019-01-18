58fc3851951114fe3ee639e0a238c79ee436f2db
Zac Fukuda <zac.s.fukuda@gmail.com>
2019-01-18 11:33:02 +0900

    Remove unnecessary files

--- searchForm.php
+++
@@ -1,9 +0,0 @@
-<?php // searchForm
-
-global $theme_textdomain; ?>
-
-<form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="s" class="screen-reader-text"><?php _e('Search for:', $theme_textdomain); ?></label>
-	<input type="search" name="s" value="<?php echo get_search_query(); ?>" title="<?php echo esc_attr_x( 'Search for:', 'label', $theme_textdomain ); ?>"/>
-	<button type="submit" id="searchsubmit" ><?php _e('Search', $theme_textdomain); ?></button>
-</form>
