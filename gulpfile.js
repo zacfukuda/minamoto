a9d415d77af9c90db28ab458642c17281ba0a377
Zac Fukuda <zac.s.fukuda@gmail.com>
2019-01-21 18:24:42 +0900

    Upgrade to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,9 @@ const packageJSON = require('./package.json')
 
 // Import modules
 const browserSync = require('browser-sync').create()
-const gulp = require('gulp')
+const { src, dest, watch, parallel, series } = require('gulp');
+// const gulp = require('gulp')
 const	autoprefixer = require('gulp-autoprefixer')
-// const concat = require('gulp-concat')
 const gcmq = require('gulp-group-css-media-queries')
 const gulpif = require('gulp-if')
 const	plumber = require('gulp-plumber')
@@ -26,103 +26,106 @@ const isDevelopment = (argv.dev === true) ? true:false
 const isProduction = (argv.pro === true) ? true:false
 const isBrowsersyncOn = (process.argv[2] === 'server') ? true : false
 
-// Broswersync
-const reload = browserSync.reload
-
-// Source files
-const src = {
-	stylus: './src/stylus/',
-	js: './src/js/'
-}
-// Destination of output files
-const dist = {
-	css: './css/',
-	js: './js/'
+// Paths
+const paths = {
+	src: {
+		stylus: './src/stylus/',
+		js: './src/js/'
+	},
+	dist: {
+		css: './css/',
+		js: './js/'
+	}
 }
 
 // Compile files
 const compileFiles = {
 	stylus: [
-		src.stylus+'*.styl',
-		'!'+src.stylus+'_*.styl'
+		paths.src.stylus+'*.styl',
+		'!'+paths.src.stylus+'_*.styl'
 	],
-	js: src.js+'*.js'
+	js: paths.src.js+'*.js'
 }
 
 // Watch files
 const watchFiles = {
-	stylus: src.stylus + '**\/*.styl',
-	js: src.js + '**\/*.js'
+	stylus: paths.src.stylus + '**\/*.styl',
+	js: paths.src.js + '**\/*.js'
 }
 
 // Stylus
-gulp.task('stylus', () => {
-	return gulp.src(compileFiles.stylus)
+const stylusTask = () => {
+	return src(compileFiles.stylus)
 		.pipe(plumber())
 		.pipe(gulpif(isProduction, sourcemaps.init()))
 		.pipe(stylus({compress: isProduction}))
 		.pipe(gulpif(isProduction, gcmq()))
-		.pipe(autoprefixer({browsers: browsers}))
+		.pipe(autoprefixer())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(gulpif(isProduction, sourcemaps.write('.')))
-		.pipe(gulp.dest(dist.css))
+		.pipe(dest(paths.dist.css))
 		.pipe(gulpif(isBrowsersyncOn, browserSync.stream()))
-})
+}
 
 // Javascript
-gulp.task('js', () => {
-	return gulp.src(compileFiles.js)
+const jsTask = () => {
+	return src(compileFiles.js)
 		.pipe(plumber())
 		.pipe(gulpif(isProduction, sourcemaps.init()))
 		// .pipe(concat('bundle.js'))
 		.pipe(uglify())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(gulpif(isProduction, sourcemaps.write('.')))
-		.pipe(gulp.dest(dist.js))
-})
+		.pipe(dest(paths.dist.js))
+}
 
-gulp.task('js-sync', ['js'], (done) => {
+const jsSync = (cb) => {
 	browserSync.reload()
-	done()
-})
+	cb()
+}
+
+// Build unminified version of CSS & JS
+const buildUnminify = (cb) => {
+	// CSS
+	src(compileFiles.stylus)
+		.pipe(sourcemaps.init())
+		.pipe(stylus())
+		.pipe(autoprefixer())
+		.pipe(sourcemaps.write('.'))
+		.pipe(dest(paths.dist.css))
+
+	// JS
+	src(compileFiles.js)
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write('.'))
+		.pipe(dest(paths.dist.js))
+
+	cb()
+}
 
 // Watch
-gulp.task('watch', () => {
-	gulp.watch(watchFiles.stylus, ['stylus'])
-	gulp.watch(watchFiles.js, ['js'])
-})
+exports.watch = () => {
+	watch(watchFiles.stylus, stylusTask)
+	watch(watchFiles.js, jsTask)
+}
 
-// Browsersync Server
-gulp.task('server', () => {
-
-	// Init Browsersync
+exports.server = () => {
 	browserSync.init({
 		open: false,
 		proxy: packageJSON.proxy
 	})
 
-	// Watch files
-	gulp.watch(watchFiles.stylus, ['stylus'])
-	gulp.watch(watchFiles.js, ['js-sync'])
+	watch(watchFiles.stylus, stylusTask)
+	watch(watchFiles.js, series(jsTask, jsSync))
 
-	// Ultimately, you can watch all PHP file
-	// gulp.watch('**\/*.php').on('change', reload)
-})
+	// Ultimately, you can watch all PHP file,
+	// and reload browser if any change happens.
+	// The author has not tested the program below,
+	// but thinks it would work.
+	// watch('**\/*.php', (cb) => {
+	//	browserSync.reload()
+	//	cb()
+	// })
+}
 
-// Build
-gulp.task('build', ['stylus', 'js'], () => {
-
-	// Output unminified version
-	gulp.src(compileFiles.stylus)
-		.pipe(sourcemaps.init())
-		.pipe(stylus())
-		.pipe(autoprefixer({browsers: browsers}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(dist.css))
-
-	gulp.src(compileFiles.js)
-		.pipe(sourcemaps.init())
-		// .pipe(concat('bundle.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(dist.js))
-})
+exports.build = parallel(stylusTask, jsTask, buildUnminify)
