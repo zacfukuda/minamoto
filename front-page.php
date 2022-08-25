04700a7bd3750b093f3e95d8ee8472c1d1e94b8c
Zac Fukuda <zac.s.fukuda@gmail.com>
2022-08-25 11:12:25 +0900

    small fix

--- front-page.php
+++ front-page.php
@@ -4,9 +4,6 @@ remove_filter('the_content', 'wpautop');
 
 $context = Timber::get_context();
 $context['page'] = new TimberPost();
-$context['posts'] = Timber::get_posts([
-	'post_type' => 'post'
-]);
 // $context['html'] = get_the_html_content();
 
 Timber::render('front-page.twig', $context);
