55e60b5b8569941fd193139155eb08ed60ccf704
Zac Fukuda <zac.s.fukuda@gmail.com>
2023-08-11 17:10:48 +0900

    organized codes

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 remove_filter('the_content', 'wpautop');
 
 $context = Timber::get_context();
-$context['page'] = new TimberPost();
+$context['post'] = new TimberPost();
 // $context['html'] = get_the_html_content();
 
 Timber::render('front-page.twig', $context);
